d279a79c43f88a2d18e18549b5a124aeba3ebe2a
Gabriel Pragin <gabriel.pragin@clearscale.com>
2024-07-10 13:52:01 -0700

    update module source with hash revision

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "std" {
-  source =  "github.com/clearscale/tf-standards.git?ref=v1.0.0"
+  source =  "git::https://github.com/clearscale/tf-standards.git?ref=c1ef5c7b2df858153a3e6ee90d92d70783029704"
 
   accounts = [var.account]
   prefix   = var.prefix
